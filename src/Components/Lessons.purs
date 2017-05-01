629443a11f6bba1d1bc0e51ff3b13f5d2d58d5a3
mattbti <matthew.doig@bti.tv>
2017-05-01 17:26:01 -0500

    Uses bootstrap pager for navigation

--- src/Components/Lessons.purs
+++ src/Components/Lessons.purs
@@ -1,16 +1,13 @@
 module Components.Lessons where
 
 import Prelude
-
-import Data.Lens (Prism', Lens, _1, _2, _Left, _Right, only, over, set, prism, lens)
 import React as R
 import React.DOM as RD
 import React.DOM.Props as RP
 import Thermite as T
+import Data.Lens (Lens, over, set, lens)
 
--- Thermite components define a type of states.
--- As a first example, we will create a component which uses a 
--- state of type Int:
+-- State
 
 type LessonsState =
   { lessonNumber   :: Int
@@ -19,7 +16,7 @@ type LessonsState =
 lessonNumber :: forall a b r. Lens { lessonNumber :: a | r } { lessonNumber :: b | r } a b
 lessonNumber = lens _.lessonNumber (_ { lessonNumber = _ })
 
--- Here is the action type associated with our component:
+-- Actions
 
 data LessonsAction
   = First
@@ -42,33 +39,34 @@ back = over lessonNumber ((_ `mod` numberOfLessons) <<< add (numberOfLessons - 1
 last :: LessonsState -> LessonsState
 last = set lessonNumber (numberOfLessons - 1)
 
--- A component needs an initial state:
-
 initialState :: LessonsState
 initialState =
   { lessonNumber : 0 }
 
--- The state is available to the render function:
-
 render :: forall props. T.Render LessonsState props LessonsAction
 render dispatch _ _ _ =
-  [ RD.nav' [ RD.a [ RP.href "#"
-                    , RP.onClick \_ -> dispatch First
-                    ]
-                    [ RD.text "⇦" ]
-            , RD.a [ RP.href "#"
-                    , RP.onClick \_ -> dispatch Back
-                    ]
-                    [ RD.text "←" ]
-            , RD.a [ RP.href "#"
-                    , RP.onClick \_ -> dispatch Next
-                    ]
-                    [ RD.text "→" ]
-            , RD.a [ RP.href "#"
-                    , RP.onClick \_ -> dispatch Last
-                    ]
-                    [ RD.text "⇨" ]
-            ]
+  [ RD.ul [ RP.className "pager" ] 
+          [ RD.li' [ RD.a [ RP.href "#"
+                          , RP.onClick \_ -> dispatch First
+                          ]
+                          [ RD.text "⇦ First" ]
+                   ]
+          , RD.li' [ RD.a [ RP.href "#"
+                          , RP.onClick \_ -> dispatch Back
+                          ]
+                          [ RD.text "← Prev" ]
+                   ]
+          , RD.li' [ RD.a [ RP.href "#"
+                          , RP.onClick \_ -> dispatch Next
+                          ]
+                          [ RD.text "→ Next" ]
+                   ]
+          , RD.li' [ RD.a [ RP.href "#"
+                          , RP.onClick \_ -> dispatch Last
+                          ]
+                          [ RD.text "⇨ Last" ]
+                   ]
+          ]
   ]
 
 performAction :: forall props eff. T.PerformAction eff LessonsState props LessonsAction
@@ -77,10 +75,6 @@ performAction Back  _ _ = void $ T.modifyState back
 performAction Next  _ _ = void $ T.modifyState next
 performAction Last  _ _ = void $ T.modifyState last
 performAction _ _ _ = pure unit
-
--- A more interesting component allows the user to modify its state.
--- We will see how Thermite supports this via "actions" in the next
--- lesson.
 
 spec :: forall props eff. T.Spec eff LessonsState props LessonsAction
 spec = T.simpleSpec performAction render
